0d5d95452aea42ec578273dc9c95f592b0c1c379
William Meldon <wmeldon@foundationsoft.com>
2013-06-27 10:17:50 -0400

    Changed cookbooks from Submodule to Librarian-Chef
    Cookbooks are now controlled by librarian-chef.  Third-Party cookbooks
    are downloaded, site based cookbooks reside in application-cookbooks.

--- chef/cookbooks/r/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default[:R][:CRAN][:default] = "cran.r-project.org"
-default[:R][:packages] = Array.new
--- chef/cookbooks/r/recipes/GEOquery.rb
+++
@@ -1,8 +0,0 @@
-case node['platform']
-when "ubuntu", "debian"
-
-	package "libcurl4-openssl-dev" do
-		action :install
-	end
-
-end
